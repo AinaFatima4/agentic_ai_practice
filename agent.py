9028935dbdfa3036d4f743e86d1099aa6caf2a53
Aina Fatima <aina.fatima678@gmail.com>
2026-08-11 16:14:40 +0500

    Fix errors

--- agent.py
+++ agent.py
@@ -1,5 +1,4 @@
 import os
-from pydantic import BaseModel
 import json
 
 from dotenv import load_dotenv
@@ -16,10 +15,6 @@ from tools import search_knowledge_base
 load_dotenv()
 set_tracing_disabled(True)
 
-#use a pydantic class to add structure to the agent's output
-class output_structure(BaseModel):
-    output : str
-    sources : str
 
 
 #making a basic local model using the env variables
@@ -35,16 +30,9 @@ local_model = OpenAIChatCompletionsModel(
 #agent that uses the local model and acts like a reseacrh assistane
 agent = Agent(
     name='Research Assistant',
-    instructions="""Answer the user's question directly and concisely.
-                When a tool is needed:
-                1. Call the appropriate tool.
-                2. Wait for the tool result.
-                3. Use the tool result to answer the user's question.
-                4. Do not describe planned tool calls to the user.
-                5. Do not stop after calling a tool. Always provide a final answer.
-                """,
+    instructions="Act as an experienced research assistant,analyze the user's prompt and provide a dedicated researched answer with relevent sources u got " \
+    "the information from , DONT TRY TO DO EVERYTHING YOURSELF, USE A TOOL IF IT FITS THE SCENARIO",
     model=local_model,
-    tools=[calculator, get_current_time, get_employee_info , search_knowledge_base],
-    output_type=output_structure
-)
+    tools=[calculator, get_current_time, get_employee_info , search_knowledge_base]
+    )
 

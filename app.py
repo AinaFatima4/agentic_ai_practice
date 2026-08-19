e223e61d69dd46f912c4bedba32e764cd65fc510
Aina Fatima <aina.fatima678@gmail.com>
2026-08-19 15:15:35 +0500

    Add manual chat history

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import gradio as gr
-from agents import Runner, SQLiteSession, InputGuardrailTripwireTriggered
+from agents import Runner, InputGuardrailTripwireTriggered
 from agent import agent
 from logger import logger
 
-session = SQLiteSession("aina_conversation")
+
+#saving chat history to keep track of the conversation between user and agent
+chat_history = []
 
 
 async def chat(message, history):      
@@ -11,12 +13,18 @@ async def chat(message, history):
     logger.info(f"USER QUERY: {message}")
     logger.info(f"AGENT: {agent.name}")
 
+    chat_history.append({"role": "user", "content" : message})
+
     try:
-        result = await Runner.run(agent, message, session=session)
+        result = await Runner.run(agent, chat_history)
+
+        chat_history.append({"role":"assistant", "content" : result.final_output})
+
         logger.info(f"FINAL RESPONSE: {result.final_output}")
         return result.final_output
     
     except InputGuardrailTripwireTriggered:
+        chat_history.pop()
         message_to_user = "Sorry, I can only help with research, calculations, date/time, or employee questions."
         logger.info(f"BLOCKED BY GUARDRAIL: {message}")
         return message_to_user
